229a5e7e3527c01d07c5415ad1ff3d55efc169f1
Giorgi Tetunashvili <giotet1997@gmail.com>
2025-05-29 16:45:05 +0400

    Init

---
+++ content.js
@@ -0,0 +1,12 @@
+// some js here
+
+// const toggleButton = document.getElementById('toggleBtn');
+// const styleTag = document.getElementById('mainStyle');
+
+// toggleButton.addEventListener('click', function () {
+//     if (styleTag.disabled) {
+//         document.styleTag.removeAttribute("disabled");
+//     } else {
+//         document.styleTag.setAttribute("disabled", "true");
+//     }
+// });
